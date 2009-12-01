5324878e003f28fa293846a54e5fe92614bea853
hylom <hylom@silver.local>
2009-12-02 01:53:51 +0900

    clean unnecessary files

--- pyblosxom2wp.py
+++ pyblosxom2wp.py
@@ -6,21 +6,11 @@
 import sys
 import os.path
 import dircache
+import time
+import re
+import wordpress
 
-"""
-XML/XHTML import format of WordPress:
-
-<item>
- <pubDate>Wed, 30 Jan 2009 12:00:00 +0000</pubDate>
- <category>Kites</category>
- <category>Taiwan</category>
- <title>Fun times</title>
- <content:encoded><p>What great times we had...</p><p>And then Bob...</p></content:encoded>
-</item>
-<item>...
-
-( http://codex.wordpress.org/Importing_Content )
-"""
+from BeautifulSoup import BeautifulStoneSoup
 
 usage = "usage: %s <pyblosxom's_story_directory>" % sys.argv[0]
 
@@ -41,11 +31,18 @@ class Pyblosxom2wp(object):
     def __init__(self, story_dir):
         """initialization"""
         self._story_dir = story_dir
-
+        self._wxr = wordpress.WordPressWxr()
+        self._wxr.channel = wordpress.Channel(
+            title = "dailyhckr",
+            link = "http://hylom.net/",
+            base_blog_url = "http://hylom.net/",
+            pubDate = time.strftime("%a, %d %b %Y %H:%M:%S +0000", time.gmtime()),
+            language = "ja")
 
     def convert(self):
         """do convert"""
         self.scan_dir(self._story_dir)
+        print self._wxr.WriteXml()
 
 
     def read_text(self, filepath):
@@ -55,25 +52,81 @@ class Pyblosxom2wp(object):
             print >> sys.stderr, "cannot open: %s. skipping...\n" % filepath
             return None
 
-       title = f.readline()
+        title = f.readline().strip()
 
-       next = f.readline()
-       tags = []
-       while next[0] == "#":
-           if next[0:4] == "#tag":
-               tags = next[4:].split()
-           next = f.readline()
+        next = f.readline()
+        tags = []
+        while next[0] == "#":
+            if next[0:4] == "#tag":
+                tags = next[4:].split()
+            next = f.readline()
 
-       content = next + f.read()
-       f.close()
-       return (tags, title, content)
+        content = next + f.read()
+        f.close()
+        return (tags, title, content)
+
+
+    def get_categories(self, path):
+        """get categories from path"""
+        rpath = os.path.relpath(path, self._story_dir)
+        (rdir, fname) = os.path.split(rpath)
+        return rdir.split(os.path.sep)
+
+    def escape(self, match):
+        xml = match.group(0)
+
+        if xml.find("\001") != -1:
+            print >> sys.stderr, "invalid character: \\001."
+        if xml.find("\002") != -1:
+            print >> sys.stderr, "invalid character: \\002."
+        if xml.find("\003") != -1:
+            print >> sys.stderr, "invalid character: \\003."
+
+        xml = xml.replace(" ", "\001");
+        xml = xml.replace("\t", "\002");
+        xml = xml.replace("\n", "\003");
+        
+        return xml
+
+    def unescape(self, xml):
+        xml = xml.replace("\001", " ");
+        xml = xml.replace("\002", "\t");
+        xml = xml.replace("\003", "\n");
+        
+        return xml
 
 
     def text_to_xml_item(self, filepath):
         """read file and generate xml"""
-        date = getmtime(path)
-        (tags, title, content) = read_text(filepath)  # TODO: do exception proc.
-        categories = get_categories(filepath)
+        pname = os.path.basename(filepath).replace(".txt", "")
+        date = os.path.getmtime(filepath)
+        (tags, title, content) = self.read_text(filepath)  # TODO: do exception proc.
+        categories = self.get_categories(filepath)
+        date_str = time.strftime( "%Y-%m-%d %H:%M:%S", time.localtime(date) );
+        date_str_gmt = time.strftime( "%Y-%m-%d %H:%M:%S", time.gmtime(date) );
+        pubDate_str = time.strftime( "%a, %d %b %Y %H:%M:%S +0000", time.gmtime(date) );
+        tidied = content
+        tidied = tidied.replace("\r\n", "\n")
+        
+        rex = re.compile(r"<pre>.*?</pre>", re.S)
+        tidied = rex.sub(self.escape, tidied)
+
+        tidied = BeautifulStoneSoup(tidied).prettify()
+        tidied = tidied.replace("\n", "")
+        tidied = tidied.replace(",", "&#44;")
+        tidied = self.unescape(tidied)
+        
+        # add entry
+        post_item = wordpress.Item(
+            title = title,
+            pubDate = pubDate_str,
+            post_date = date_str,
+            post_date_gmt = date_str_gmt,
+            content = tidied,
+            post_name = pname)
+        post_item.tags = tags
+        post_item.categories = categories
+        self._wxr.channel.items.append(post_item)
 
     def scan_dir(self, dir):
         """scan dir and proccess"""
@@ -81,11 +134,11 @@ class Pyblosxom2wp(object):
         # get files
         files = dircache.listdir(dir)
         for file in files:
-            path = os.path.join(story_dir, file)
+            path = os.path.join(dir, file)
             if os.path.isdir(path):
-                scan_dir(path)
+                self.scan_dir(path)
             else:
-                text_to_xml_item(path)
+                self.text_to_xml_item(path)
 
 
 main()
